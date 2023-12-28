4f90541b85ce09734206393e3d0e43db8ea1cadb
Nadelio <carefulsniffle@gmail.com>
2023-12-28 11:44:43 -0800

    Customization and Interaction setup

--- Main.java
+++ Main.java
@@ -1,6 +1,10 @@
 import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 public class Main extends JFrame
@@ -12,26 +16,43 @@ public class Main extends JFrame
 
     public Main()
     {
-        JPanel panel = new JPanel();
+        // create components (label and button)
         JButton button = new JButton("Testing");
+        JLabel label = new JLabel("PlaceholderText");
 
-        panel.setBounds(0, 0, 220, 30);
+        // set the bounds of label and button
         button.setBounds(400, 650, 200, 100);
+        label.setBounds(450, 400, 200, 100);
 
+        // set layouts of panel and frame to null
         setLayout(null);
-        panel.setLayout(null);
 
+        // configure button settings
+        button.setBackground(Color.DARK_GRAY);
+        button.setForeground(Color.WHITE);
+
+        // configure label settings
+        label.setForeground(Color.WHITE);
+
+        // configure Frame settings
         setSize(1000, 800);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setBackground(Color.BLACK);
+        getContentPane().setBackground(Color.BLACK);
         setTitle("Window and Interaction Prototype");
         setLocationRelativeTo(null);
         setVisible(true);
 
-        add(panel);
-        panel.add(button);
+        // add everything
         add(button);
+        add(label);
 
-        panel.setLocation(0,0);
+        // setup an action listener to button
+        button.addActionListener(new ActionListener(){
+            @Override
+            public void actionPerformed(ActionEvent e)
+            {
+                label.setText("You did something!");
+            }
+        });
     }
 }
